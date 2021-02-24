e1c7e3cf06fe82d5e2337f8d2faa9111d3dec521
Josh Goldberg <josh@codecademy.com>
2021-02-24 06:06:51 -0500

    Convert to promises

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const jokeCards = document.getElementById("jokeCards");
 const reloader = document.getElementById("reload");
 
-const loadJokes = async () => {
+const loadJokes = () => {
   jokeCards.textContent = "Loading...";
 
-  const response = await fetch("https://official-joke-api.appspot.com/jokes/ten");
-  const jokes = await response.json();
+  return fetch("https://official-joke-api.appspot.com/jokes/ten")
+    .then((response) => response.json())
+    .then((jokes) => {
+      jokeCards.textContent = "";
 
-  jokeCards.textContent = "";
+      for (const joke of jokes) {
+        const card = document.createElement("div");
+        card.className = "card";
 
-  for (const joke of jokes) {
-    const card = document.createElement("div");
-    card.className = "card";
+        card.addEventListener("click", () => {
+          card.className = card.className === "card" ? "card expanded" : "card";
+        });
 
-    card.addEventListener("click", () => {
-      card.className = card.className === "card" ? "card expanded" : "card";
+        const setup = document.createElement("h2");
+        setup.textContent = joke.setup;
+        card.appendChild(setup);
+
+        const punchline = document.createElement("p");
+        punchline.textContent = joke.punchline;
+        card.appendChild(punchline);
+
+        jokeCards.appendChild(card);
+      }
+    })
+    .catch((error) => {
+      console.error("Oh no!", error);
     });
-
-    const setup = document.createElement("h2");
-    setup.textContent = joke.setup;
-    card.appendChild(setup);
-
-    const punchline = document.createElement("p");
-    punchline.textContent = joke.punchline;
-    card.appendChild(punchline);
-
-    jokeCards.appendChild(card);
-  }
 };
 
 reloader.addEventListener("click", loadJokes);
 
-loadJokes().catch((error) => {
-  console.error("Oh no!", error);
-});
+loadJokes();
--- index_bonus.js
+++ index_bonus.js
@@ -4,44 +4,48 @@ const selector = document.getElementById("selector");
 
 const seen = {};
 
-const loadJokes = async () => {
+const loadJokes = () => {
   jokeCards.textContent = "Loading...!";
-  
-  const response = await fetch(`https://official-joke-api.appspot.com/jokes/${selector.value}/ten`);
-  const jokes = await response.json();
 
-  jokeCards.textContent = "";
+  return fetch(`https://official-joke-api.appspot.com/jokes/${selector.value}/ten`)
+    .then((response) => response.json())
+    .then((jokes) => {
+      const jokes = response.json();
 
-  for (const joke of jokes) {
-    if (seen[joke.setup]) {
-      continue;
-    }
+      jokeCards.textContent = "";
 
-    seen[joke.setup] = true;
+      for (const joke of jokes) {
+        if (seen[joke.setup]) {
+          continue;
+        }
 
-    const card = document.createElement("button");
-    card.className = "card";
+        seen[joke.setup] = true;
 
-    card.addEventListener("click", () => {
-      card.className = card.className === "card" ? "card expanded" : "card";
+        const card = document.createElement("button");
+        card.className = "card";
+
+        card.addEventListener("click", () => {
+          card.className = card.className === "card" ? "card expanded" : "card";
+        });
+
+        const setup = document.createElement("h2");
+        setup.textContent = joke.setup;
+        card.appendChild(setup);
+
+        const punchline = document.createElement("p");
+        punchline.textContent = joke.punchline;
+        card.appendChild(punchline);
+
+        jokeCards.appendChild(card);
+      }
+    })
+    .catch((error) => {
+      console.error("Oh no!", error);
     });
-
-    const setup = document.createElement("h2");
-    setup.textContent = joke.setup;
-    card.appendChild(setup);
-
-    const punchline = document.createElement("p");
-    punchline.textContent = joke.punchline;
-    card.appendChild(punchline);
-
-    jokeCards.appendChild(card);
-  }
 };
 
 reloader.addEventListener("click", loadJokes);
 
 selector.addEventListener("change", loadJokes);
 
-loadJokes().catch((error) => {
-  console.error("Oh no!", error);
-});
+loadJokes();
