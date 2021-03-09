bea2d9e97e30c16cf7f29e27f81c5e7582cffc0d
Alexander <alexanderl96@yahoo.com>
2021-03-09 17:42:39 -0500

    2

--- index.js
+++ index.js
@@ -3,39 +3,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
 .then(response =>{
     return response.json()
 }).then(body =>{
-    console.log(body)
-    const random = body.setup
+    const random = body.setup.
     let sec = body.punchline
     const ul = document.querySelector('ul')
     console.log(random)
     const li = document.createElement('li')
-    li.textContent = random  `Punchline: ${sec}`
+    li.textContent = `${random}` +  " " + `Punchline: ${sec}`
     ul.appendChild(li)
     console.log(sec)
-    // const second = document.getElementById('second')
-    // second.addEventListener("click", ()=>{
-    //  const secondli = document.createElement('li')
-    //  secondli.textContent = sec
-    //  second.appendChild(secondli)
-    //     if(li.textContent !== sec){
-    //         e.textContent = random + sec
-    //     }
-    // })
-    
- 
 })
 })
-
-// const secondli = document.createElement('li')
-    // secondli.textContent = sec
-    // second.appendChild(secondli)
-
-    // console.log(li)
-
-    // ul.appendChild(li)
-// for(let user of random){
-//     const li = document.createElement('li')
-//     li.textContent = user.punchline
-//     ul.appendChild(li)
-//     console.log(user.text)
-// }
