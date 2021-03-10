c4ce8ba0a1d490aaefda89f5e31daa3110df15e9
Alexander <alexanderl96@yahoo.com>
2021-03-10 01:03:27 -0500

    34

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', e =>{
+    e.preventDefault()
+    jokes()
+
+    const button = document.getElementById('reload')
+    button.addEventListener('click', ()=>{
+            const rel = document.getElementById('btn')
+            rel.textContent = 'btn'
+            space.innerHTML = ''
+    })
+   
+})
+function jokes(){
+    const space = document.getElementById('space')
     fetch('https://official-joke-api.appspot.com/jokes/random')
-.then(response =>{
+    .then(response =>{
     return response.json()
-}).then(body =>{
-    const random = body.setup
-    let sec = body.punchline
-    const ul = document.querySelector('ul')
-    console.log(random)
-    const li = document.createElement('li')
-    li.textContent = `${random}` +  " " + `Punchline: ${sec}`
-    ul.appendChild(li)
-    console.log(sec)
-})
-})
+    }).then(body =>{
+        const random = body.setup
+        const sec = body.punchline
+        const ul = document.querySelector('.card')
+        const li = document.createElement('li') 
+        li.className = 'setup'
+        li.textContent = random
+        ul.appendChild(li)
+
+        const res = document.createElement('li')
+        res.className = 'punchline'
+        res.style.visibility = 'hidden'
+        res.textContent = `Punchline: ${sec}`
+        ul.appendChild(res)
+
+        space.appendChild(ul)
+
+        li.addEventListener("click", () =>{
+            if(res.style.visibility === 'hidden'){
+                res.style.visibility = 'visible'
+            }else{
+                res.style.visibility = 'hidden'
+            }
+        })
+            
+    })
+ 
+    
+    
+}  
+
