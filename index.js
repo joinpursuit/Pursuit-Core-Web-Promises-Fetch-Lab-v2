40b4285254b554abe78ce48641ab84cd30edd0cb
Alexander <alexanderl96@yahoo.com>
2021-03-11 00:18:39 -0500

    all

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
+
+
 document.addEventListener('DOMContentLoaded', e =>{
     e.preventDefault()
     jokes()
 
+   
+})
+function jokes(){ 
+    const space = document.getElementById('space')
+    fetch('https://official-joke-api.appspot.com/random_ten')
+    .then(response =>{
+    return response.json()
+    }).then(body =>{
+        for(let te of body){
+            const forms = document.createElement('div')
+            forms.className = 'card'
+            const random = document.createElement('span')
+            random.className = 'setup'
+            random.textContent = te.setup
+            console.log(random)
+            forms.appendChild(random)
+
+            const answer = document.createElement('span')
+            answer.className = 'punchline'
+            answer.style.visibility = 'hidden'
+            answer.textContent = `Punchline: ${te.punchline}`
+            console.log(answer)
+            forms.appendChild(answer)
+
+            space.appendChild(forms)
+
+            random.addEventListener("click", e =>{
+                e.preventDefault()
+                if(answer.style.visibility === 'hidden'){
+                    answer.style.visibility = 'visible'
+                }else{
+                    answer.style.visibility = 'hidden'
+                }
+            })
+
+        }
+        
+    })
+    
     const button = document.getElementById('reload')
     button.addEventListener('click', ()=>{
             const rel = document.getElementById('btn')
             rel.textContent = 'btn'
             space.innerHTML = ''
     })
+}   
+       
+     
    
-})
-function jokes(){
-    const space = document.getElementById('space')
-    fetch('https://official-joke-api.appspot.com/jokes/random')
-    .then(response =>{
-    return response.json()
-    }).then(body =>{
-        const random = body.setup
-        const sec = body.punchline
-        const ul = document.querySelector('.card')
-        const li = document.createElement('li') 
-        li.className = 'setup'
-        li.textContent = random
-        ul.appendChild(li)
-
-        const res = document.createElement('li')
-        res.className = 'punchline'
-        res.style.visibility = 'hidden'
-        res.textContent = `Punchline: ${sec}`
-        ul.appendChild(res)
-
-        space.appendChild(ul)
-
-        li.addEventListener("click", () =>{
-            if(res.style.visibility === 'hidden'){
-                res.style.visibility = 'visible'
-            }else{
-                res.style.visibility = 'hidden'
-            }
-        })
-            
-    })
  
     
-    
-}  
 
