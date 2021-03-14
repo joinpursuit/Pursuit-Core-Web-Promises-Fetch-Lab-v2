40504a126c9812e099cd32534d6636dd2f9946d1
joshuamendez <joshuafmendez@gmail.com>
2021-03-14 14:06:04 -0400

    implemented show and hide for punchline

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const section = document.querySelector('section');
     // const body= document.body
-    const jb = document.querySelector('#jokes-button');
+    const jb = document.querySelector('#reload');
+    // possible set timer
     const loadJokes = () =>{
         fetch('https://official-joke-api.appspot.com/random_ten')
         .then(res => {
@@ -9,35 +10,26 @@ document.addEventListener('DOMContentLoaded', () => {
             if(!res.ok){
                 throw Error('Something went wrong')
             }
-            section.innerHTML = ""
+            section.innerHTML = "";
             return res.json();
         }).then(res => {
-            // debugger
             for(let i = 0; i < res.length; i++){
-                // This is set up of joke
-                // console.log(res[i].setup);
                 const card = document.createElement('span');
                 card.classList.add('card');
                 card.textContent = res[i].setup;
                 section.appendChild(card);
-                card.addEventListener("click", (e) => {
-                    const card = e.target
-                    const punchline = document.createElement("span")
-                    punchline.classList.add('punchline')
-                    //console.log(punchline)
-                    if(punchline.textContent !== res[i].punchline) {
-                        punchline.textContent = res[i].punchline
-                    card.appendChild(punchline)
-
-                } else {
-                   punchline.textContent = ""
-                    console.log("help")
-                }
-
+                const punchline = document.createElement("span")
+                punchline.classList.add('punchline')
+                punchline.textContent = res[i].punchline;
+                card.appendChild(punchline);
+                punchline.style.visibility = 'hidden'
+                card.addEventListener("click", () => {
+                    if(punchline.style.visibility === 'hidden') {
+                        punchline.style.visibility = 'visible';
+                    } else {
+                        punchline.style.visibility = 'hidden';
+                    }
                 })
-                
-                // this is punch line of joke
-                // console.log(res[i].punchline);
             }
         }).catch(err => {
             console.log(err);
