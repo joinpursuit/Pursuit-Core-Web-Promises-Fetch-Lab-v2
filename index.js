c1dd0924c9ecf0e8c6a1165a3957660c36d4bbeb
Daliz Cruz <dalizcruz@gmail.com>
2021-03-14 11:49:44 -0400

    bullet point 1 done

---
+++ index.js
@@ -0,0 +1,18 @@
+//A list of ten jokes that are randomly loaded when the page is refreshed, each with the `"card"` class
+const ul = document.querySelector("ul")
+fetch("https://official-joke-api.appspot.com/random_ten").then((res) => {
+  if (!res.ok) {
+    throw Error("error!");
+  }
+  return res.json();
+}).then((res) =>{
+   res.forEach(jokes =>{
+    const li = document.createElement("li")
+    li.textContent = jokes.setup
+    li.classList.add("card");
+    ul.appendChild(li);
+
+   })
+
+    
+})
