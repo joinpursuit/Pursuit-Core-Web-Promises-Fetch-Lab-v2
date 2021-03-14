d9c2893c9d8e74a15baf3b2840118dd1479e098b
Flower Aviles <itsfloweraviles@gmail.com>
2021-03-14 15:54:26 -0400

    We tried

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const jokes = document.querySelector("#jokes");
+const ul = document.querySelector("#jokes");
+const punchline = document.querySelector("#punchline")
 const button = document.querySelector("#refreshBut");
 const body = document.body;
 
-window.addEventListener("load", (e) => {
-  fetch("https://official-joke-api.appspot.com/jokes/ten")
-    .then((res) => {
-      if (!res.ok) {
-        throw Error("Something went wrong.");
-      }
-      return res.json();
+
+  fetch("https://official-joke-api.appspot.com/random_ten")
+  .then(response => {
+    console.log(response.json)
+    return response.json();
+  }).then(res => {
+    res.forEach(jokes => {
+    const li = document.createElement("li")
+    li.innerText = jokes.setup
+    ul.appendChild(li) 
+    const li2 = document.createElement("li")
+    li2.innerText = jokes.punchline
+    li.addEventListener("click" ,(event) => {
+      li.appendChild(li2)
+      //  li2.style.visibility = "hidden"
+    
+      // if(event.target.style.visibility === "hidden"){
+      //   event.target.style.visibility ="visible"
+      // } else {
+      //   event.target.style.visibility = "hidden"
+      // }
+   
+    // li2.style.visibility = "visible"
+    
+      // if(event.target.style.visbility === "hidden"){
+      //   event.target.style.visbility ="visible"
+      // } else {
+      //   event.target.style.visbility = "hidden"
+      // }
     })
-}).catch((err) => {
-    console.log(err);
-})
+    })
+    })
+    
+  
+
+
+  
