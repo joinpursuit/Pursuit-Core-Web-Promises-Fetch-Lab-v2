9bb95bac5199373688de85f76f56f684952c855e
Durdona Djalilova <ddjalilova3@gmail.com>
2021-03-14 12:05:40 -0400

    halfway there

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const section = document.querySelector('section');
+    // const body= document.body
     const jb = document.querySelector('#jokes-button');
-    jb.addEventListener('click', ()=>{
+    const loadJokes = () =>{
         fetch('https://official-joke-api.appspot.com/random_ten')
         .then(res => {
+
             if(!res.ok){
                 throw Error('Something went wrong')
             }
+            section.innerHTML = ""
             return res.json();
         }).then(res => {
             // debugger
@@ -17,11 +20,33 @@ document.addEventListener('DOMContentLoaded', () => {
                 card.classList.add('card');
                 card.textContent = res[i].setup;
                 section.appendChild(card);
+                card.addEventListener("click", (e) => {
+                    const card = e.target
+                    const punchline = document.createElement("span")
+                    punchline.classList.add('punchline')
+                    //console.log(punchline)
+                    if(punchline.textContent !== res[i].punchline) {
+                        punchline.textContent = res[i].punchline
+                    card.appendChild(punchline)
+
+                } else {
+                   punchline.textContent = ""
+                    console.log("help")
+                }
+
+                })
+                
                 // this is punch line of joke
                 // console.log(res[i].punchline);
             }
         }).catch(err => {
             console.log(err);
         })
-    })
+    }
+
+    loadJokes()
+
+    
+
+    jb.addEventListener('click', loadJokes)
 });
