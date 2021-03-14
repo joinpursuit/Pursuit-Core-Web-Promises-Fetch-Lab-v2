04f6f677aa20e1b9802b85f18925286aa6bf6061
Jonathan Bedon <bedonjonathan@gmail.com>
2021-03-14 16:35:54 -0400

    One bones spec left

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const button = document.querySelector("#reload");
 let ul = document.querySelector("ul");
 const select = document.querySelector("select");
-let endPoint = "/ten";
+let endPoint = "/*/ten"; // /ten -> for regu test specs and /*/ten -> for bonus specs
+let usedJokes =[];
 const innitLoad = () => {
+  ul.innerHTML = "";
   fetch(`https://official-joke-api.appspot.com/jokes${endPoint}`)
     .then((res) => {
       if (!res.ok) {
@@ -12,13 +14,20 @@ const innitLoad = () => {
     })
     .then((res) => {
       res.forEach((joke) => {
+        if( usedJokes.includes(joke.id)){
+
+        }else{
+          
+        }
+
         const div = document.createElement("div");
         div.className = "card";
         const h2 = document.createElement("h2");
         h2.textContent = joke.setup;
-        // h2.textContent = joke.setup;
+        // h2.textContent = joke.type;
         const h5 = document.createElement("h5");
         h5.textContent = joke.punchline;
+        usedJokes.push(joke.id)
         div.appendChild(h2);
         div.addEventListener("click", (e) => {
           if (div.innerHTML.includes("h5")) {
@@ -36,21 +45,16 @@ innitLoad();
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  ul.innerHTML = "";
   innitLoad();
 });
 select.addEventListener("change", (e) => {
-    const category = e.target.value;
-    // debugger
-    if (category === "general") {
-      endPoint = "/general/ten";
-    //   debugger
-    } else if (category === "programming") {
-      endPoint = "/programming/ten";
-    //   debugger
-    }else {
-      endPoint = "/ten" ;
-    //   debugger
-    }
-
-  });
+  const category = e.target.value;
+  if (category === "general") {
+    endPoint = "/general/ten";
+  } else if (category === "programming") {
+    endPoint = "/programming/ten";
+  } else {
+    endPoint = "/ten";
+  }
+  innitLoad()
+});
