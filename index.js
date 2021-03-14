00e54d8664e32c38374d6c603bf3ac0a8f3f59a7
Jonathan Bedon <bedonjonathan@gmail.com>
2021-03-14 12:07:03 -0400

    Cards fully funtional

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const button = document.querySelector("#reload-btn");
-let ul = document.querySelector("#")
+const button = document.querySelector("#reload");
+let ul = document.querySelector("ul");
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  fetch("https://official-joke-api.appspot.com/random_ten").then(res => {
-      if(!res.ok) {
-          throw Error(`Something wrong ${res.status}`)
-        }
-        return res.json()
-    }).then(res => {
-      res.set
-  })
+  ul.innerHTML = ""
+  fetch("https://official-joke-api.appspot.com/random_ten")
+    .then((res) => {
+      if (!res.ok) {
+        throw Error(`Something wrong ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((res) => {
+      res.forEach((joke) => {
+        const div = document.createElement("div");
+        div.className = "card";
+        const h2 = document.createElement("h2");
+        h2.textContent = joke.setup;
+        const h5 = document.createElement("h5");
+        h5.textContent = joke.punchline;
+        div.appendChild(h2);
+        div.addEventListener("click", (e) =>{
+          if(div.innerHTML.includes("h5")){
+            div.removeChild(h5)
+          }else{
+            div.appendChild(h5);
+          }
+        })
+        ul.appendChild(div);
+      });
+    });
+
 });
