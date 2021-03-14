03561be0ce8997ed8935dc3494aa935188668a53
Cari <t.carina@rocketmail.com>
2021-03-14 17:06:00 -0400

    got expand to work

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const ul = document.querySelector("#jokes");
-const punchline = document.querySelector("#punchline")
+const punchline = document.querySelector("#punchline");
 const button = document.querySelector("#refreshBut");
 const body = document.body;
 
-
-  fetch("https://official-joke-api.appspot.com/random_ten")
-  .then(response => {
-    console.log(response.json)
+fetch("https://official-joke-api.appspot.com/random_ten")
+  .then((response) => {
+    console.log(response.json);
     return response.json();
-  }).then(res => {
-    res.forEach(jokes => {
-    const li = document.createElement("li")
-    li.innerText = jokes.setup
-    ul.appendChild(li) 
-    const li2 = document.createElement("li")
-    li2.innerText = jokes.punchline
-    li.appendChild(li2)
-    li2.style.display ="none"
-    li.addEventListener("click" ,(event) => {
-      if (event.target.childNodes[1].style.display === "none") {
-          event.target.childNodes[1].style.display = "block"
-      } else {
-        event.target.childNodes[1].style.display = "none"
-      }
-  const refresh = () => {
-location.reload()
-  }
+  })
+  .then((res) => {
+    res.forEach((jokes) => {
+      const li = document.createElement("li");
+      li.innerText = jokes.setup;
+      ul.appendChild(li);
+      const li2 = document.createElement("li");
+      li2.innerText = jokes.punchline;
+      li.appendChild(li2);
+      li2.style.display = "none";
+      li.addEventListener("click", (event) => {
+        if (event.target.childNodes[1].style.display === "none") {
+          event.target.childNodes[1].style.display = "block";
+        } else {
+          event.target.childNodes[1].style.display = "none";
+        }
+      });
+    });
+  });
 
-
-  
+const refresh = () => {
+  location.reload();
+};
