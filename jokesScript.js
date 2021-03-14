78a60f0273467bc8e97d43fe61ece9326ecc8a31
Marsha Hall <mphall77@gmail.com>
2021-03-14 11:16:10 -0400

    add css

--- jokesScript.js
+++ jokesScript.js
@@ -2,6 +2,8 @@ console.log("hey");
 
 const button = document.querySelector("#reload");
 const section = document.querySelector("section");
+const test = document.querySelector("h3");
+
 
 button.addEventListener("click", (e) => {
 	// get 10 jokes from the API
@@ -12,19 +14,38 @@ button.addEventListener("click", (e) => {
 			}
 			return res.json();
 		})
-		.then((res) => {
-			console.log(res);
-            // need to loop each joke
-            res.forEach(joke => {
-                const p = document.createElement("p")
-                p.textContent = joke.setup;
-                section.appendChild(p)
-            });
-		})
+        .then((res) => {
+					// clear the old jokes
+					section.innerHTML = "";
+					console.log(res);
+					// need to loop each joke
+					res.forEach((joke) => {
+						// create a p tag for each joke
+						const p = document.createElement("p");
+						// add the class
+						p.className = "card";
+						p.textContent = joke.setup;
+						// append each joke to the section tag
+						section.appendChild(p);
+
+						// punchline
+						const punch = document.createElement("h5");
+						punch.textContent = joke.punchline;
+						p.appendChild(punch);
+					});
+					
+				})
 		.catch((err) => {
 			console.log(err);
 		});
-	// clear the old jokes
-	// create a p tag for each joke
-	// append each joke to the section tag
 });
+
+section.addEventListener("onclick", e => {
+    h3.style.visibility = visible;
+    h1.style.color = red;
+})
+// const cardList = document.querySelectorAll(".card");
+// console.log(cardList);
+// cardList.addEventListener("click", (e) => {
+// 	h5.style.visibility = visible;
+// });
