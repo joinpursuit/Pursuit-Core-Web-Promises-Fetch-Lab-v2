e01c51c9123c77d234b7304191565f5a380d1ca4
cassidybeni <cassidybeni@pursuit.org>
2021-03-14 22:34:11 -0400

    Create html and js files

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    fetch('https://official-joke-api.appspot.com/random_ten').then(res => {
+        return res.json
+    }).then(res => {
+
+    })
+    
+})
