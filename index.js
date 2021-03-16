3558100c1e4e395c4dde2d286fdcae347d62742f
Shoaib Dar <SDAR30@gmail.com>
2021-03-15 21:53:36 -0400

    Async update

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const button = document.querySelector("#reload");
 const list = document.querySelector("#list");
 
-button.addEventListener("click", (e) => {
-    loadJokes()
-})
 
-const loadJokes = () => {
+const loadJokes = async (e) => {
     const select = document.getElementById("select");
     let str = "";
     if (select.value === "general") {
@@ -15,34 +12,34 @@ const loadJokes = () => {
     } else {
         str = "https://official-joke-api.appspot.com/random_ten"
     }
-    fetch(str)
-        .then(res => {
-            return res.json();
-        }).then(res => {
-            list.innerHTML = "";
-            res.forEach((joke, i) => {//forEach
-                let li = document.createElement("li")
-                let div = document.createElement("div")
-                div.textContent = joke.setup;
-                li.className = "card"
-                li.append(div)
-                list.append(li)
-                let p = document.createElement("p")
-                p.textContent = joke.punchline;
-                p.style.visibility = "hidden";
-                li.appendChild(p)
+    const URL = await axios.get(str)
+    debugger
+    list.innerHTML = "";
+    URL.data.forEach((joke, i) => {//forEach
+        let li = document.createElement("li")
+        let div = document.createElement("div")
+        div.textContent = joke.setup;
+        li.className = "card"
+        li.append(div)
+        list.append(li)
+        let p = document.createElement("p")
+        p.textContent = joke.punchline;
+        p.style.visibility = "hidden";
+        li.appendChild(p)
 
-                li.addEventListener("click", (e) => {//eventListener
-                    if (p.style.visibility === "hidden") {
-                        p.style.visibility = "visible";
-                    } else {
-                        p.style.visibility = "hidden";
-                    }
-                })//eventListener
-            })//forEach
-        })
+        li.addEventListener("click", (e) => {//eventListener
+            if (p.style.visibility === "hidden") {
+                p.style.visibility = "visible";
+            } else {
+                p.style.visibility = "hidden";
+            }
+        })//eventListener
+    })//forEach
 }
 
 loadJokes()
+
+button.addEventListener("click", loadJokes)
+
 
 
