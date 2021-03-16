4278d7eb1a68fff406cd57d39a3cdef8100bed3f
vrwatson <vanessawatson@pursuit.org>
2021-03-15 21:56:12 -0400

    axios refactoring

--- index.js
+++ index.js
@@ -1,45 +1,38 @@
 const ul = document.querySelector("#joke-list");
 
-const createJokes = () => {
-  fetch("https://official-joke-api.appspot.com/random_ten")
-    .then((res) => {
-      if (!res.ok) {
-        throw Error("Not today Satan!");
-      }
-      return res.json();
-    })
-    .then((res) => {
-      res.forEach((joke) => {
-        const li = document.createElement("li");
-        ul.appendChild(li);
-        li.classList.add("card");
-        const setup = document.createElement("p")
-        setup.textContent = joke.setup;
-        li.appendChild(setup)
-        const punchline = document.createElement("p");
-        punchline.textContent = joke.punchline;
-        punchline.style.display = "none";
-        punchline.classList.add("punchline")
-        li.appendChild(punchline);
-        li.addEventListener("click", (e) => {
-          if (punchline.style.display === "block") {
-            punchline.style.display = "none"
-          } else {
-            punchline.style.display = "block"
-          }
-        });
+const createJokes = async () => {
+  try {
+    const res = await axios.get(
+      "https://official-joke-api.appspot.com/random_ten"
+    );
+    res.data.forEach((joke) => {
+      const li = document.createElement("li");
+      ul.appendChild(li);
+      li.classList.add("card");
+      const setup = document.createElement("p");
+      setup.textContent = joke.setup;
+      li.appendChild(setup);
+      const punchline = document.createElement("p");
+      punchline.textContent = joke.punchline;
+      punchline.style.display = "none";
+      punchline.classList.add("punchline");
+      li.appendChild(punchline);
+      li.addEventListener("click", (e) => {
+        if (punchline.style.display === "block") {
+          punchline.style.display = "none";
+        } else {
+          punchline.style.display = "block";
+        }
       });
-    })
-    .catch((err) => {
-      console.log(err);
     });
+  } catch (err) {
+    console.log(err);
+  }
 };
 createJokes();
 
-
-const button = document.querySelector("button")
-button.addEventListener("click", (e) =>{
-  ul.textContent = ""
-  createJokes()
-})
-
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+  ul.textContent = "";
+  createJokes();
+});
