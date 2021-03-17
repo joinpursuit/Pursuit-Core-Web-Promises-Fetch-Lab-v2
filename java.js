821bfb733491b3a633c792a3e7a930c3ebdafa4c
Jamee Hengari <jhengari@hotmail.com>
2021-03-17 17:12:13 -0400

    second promise lab commit

--- java.js
+++ java.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let card = document.querySelector(".card");
+  let jokes = document.getElementById("jokes");
   fetch("https://official-joke-api.appspot.com/jokes/ten").then((response) => {
-    return response.json().then((jokes) => {
-        card.textContent = jokes
-    })
+    return response.json().then((body) => {
+      body.forEach((joke) => {
+        let card = document.createElement("li");
+        card.setAttribute("class", "card");
+        card.textContent = joke.setup;
+        jokes.appendChild(card);
+        let p = document.createElement("p");
+        card.addEventListener("click", (event) => {
+          if (p.textContent.length === 0) {
+            p.textContent = joke.punchline;
+            event.target.appendChild(p);
+          } else if (p.textContent.length > 0) {
+            p.textContent = "";
+          }
+        });
+      });
+    });
   });
 });
