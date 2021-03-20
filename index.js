066f3f3e4b5ce30206f1f400e7364e0d16cf6d73
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-03-20 00:16:01 -0400

    review

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-fetch('https://official-joke-api.appspot.com/random_ten')
-    .then(response => {
-  return response.json()
-}).then(body => {
+document.addEventListener('DOMContentLoaded', () => {
+    Jokes()
 
-  const userList = document.querySelector('ul')
-  for (let user of body) {
-    const userItem = document.createElement('li')
-    userItem.textContent = user.setup
-    userList.appendChild(userItem)
-  }
+const button = document.querySelector("#reload")
+const ul = document.querySelector(".cards")
+
+
+
+
+
+
+    function Jokes() {
+    const ul = document.querySelector(".cards")
+
+    
+    fetch("https://official-joke-api.appspot.com/random_ten").then(response => {
+        return response.json()
+        }).then(body => {   
+        for (i = 0; i < 10; i++) {
+        let div = document.createElement("div")
+            div.classList.add('card')
+            div.setAttribute("type", "button")
+            ul.appendChild(div)
+        let setUp = document.createElement('p')
+            setUp.classList.add('setUp')
+            setUp.innerHTML = body[i].setup
+            div.appendChild(setUp)
+        let ans = document.createElement('p')
+            ans.classList.add('punchLine')
+            ans.innerHTML = body[i].punchline
+            div.appendChild(ans)
+         div.addEventListener('click', () => {
+            if (ans.style.visibility === "visible"){
+             ans.style.visibility = "hidden"
+            }else{
+             ans.style.visibility = "visible"
+                    }
+                })
+            }
+        })
+    }
+    button.addEventListener('click', () => {
+        ul.textContent = ''
+        Jokes()
+    })
 })
