aeef28128f5e02cd364403abdc7e299b81f8fb9d
Talia Pichardo <taliapichardo@gmail.com>
2021-03-25 13:13:21 -0400

    section refreshes on click of btn

--- lab.js
+++ lab.js
@@ -1,33 +1,38 @@
-const displaySection = document.querySelector("div");
-
-//add somthing to reset it so that the jokes don't keep getting added
-document.querySelector("#reload").addEventListener("click", getFirstPart);
+const displaySection = document.querySelector(".container");
+document.querySelector("#reload").addEventListener("click", (jokes));
 
 
-function getFirstPart(){
-  // e.preventDefault()
+function jokes(){
   fetch("https://official-joke-api.appspot.com/random_ten").then(response=>{
     return response.json()
   }).then(body =>{
     console.log(body)
-   let joke = body
-   for(let firstPart of joke){
-     const heading = document.createElement("h3")
-     const punchlines = document.createElement("p")
-     heading.classList.add("card")
-     heading.textContent = firstPart.setup
-     punchlines.textContent = firstPart.punchline
-
-     displaySection.appendChild(heading)
-     displaySection.appendChild(punchlines)
-     heading.addEventListener("click", ()=>{
-       if(punchlines.style.display === "none"){
-        punchlines.style.display = "block"
-       }else{
-         punchlines.style.display = "none"
-       }
-     })
-   }
+    let joke = body
+    for(let jokes of joke){
+      const section = document.createElement("div")
+      const heading = document.createElement("h3")
+      const punchlines = document.createElement("p")
+      section.classList.add("card")
+      heading.textContent = jokes.setup
+      punchlines.textContent = jokes.punchline
+      
+      section.appendChild(heading);
+      section.appendChild(punchlines)
+      displaySection.appendChild(section)
+      
+      heading.addEventListener("click", ()=>{
+        if(punchlines.style.display === "none"){
+          punchlines.style.display = "block"
+        }else{
+          punchlines.style.display = "none"
+        }
+      })
+    }
+    
   })
+  displaySection.innerHTML = ''
+    
+};
 
-}
+
+
