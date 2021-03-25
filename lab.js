9ea0f05220016822ba0d1e38d24bde7723c6f4dd
Talia Pichardo <taliapichardo@gmail.com>
2021-03-25 00:06:27 -0400

    added setup and punchline

--- lab.js
+++ lab.js
@@ -1,11 +1,33 @@
-const ul = document.querySelector("ul");
-const reloadBtn = document.querySelector("#reload");
+const displaySection = document.querySelector("div");
 
-fetch("https://official-joke-api.appspot.com/random_ten").then(response=>{
-  return response.json()
-}).then(body =>{
- let joke = body
- for(let firstPart of joke){
-   console.log(firstPart.setup)
- }
-})
+//add somthing to reset it so that the jokes don't keep getting added
+document.querySelector("#reload").addEventListener("click", getFirstPart);
+
+
+function getFirstPart(){
+  // e.preventDefault()
+  fetch("https://official-joke-api.appspot.com/random_ten").then(response=>{
+    return response.json()
+  }).then(body =>{
+    console.log(body)
+   let joke = body
+   for(let firstPart of joke){
+     const heading = document.createElement("h3")
+     const punchlines = document.createElement("p")
+     heading.classList.add("card")
+     heading.textContent = firstPart.setup
+     punchlines.textContent = firstPart.punchline
+
+     displaySection.appendChild(heading)
+     displaySection.appendChild(punchlines)
+     heading.addEventListener("click", ()=>{
+       if(punchlines.style.display === "none"){
+        punchlines.style.display = "block"
+       }else{
+         punchlines.style.display = "none"
+       }
+     })
+   }
+  })
+
+}
